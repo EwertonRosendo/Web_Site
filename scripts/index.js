361cbc3395b0d2ca012cd88be696e461a6bc9659
Skagth <fangame147@hotmail.com>
2022-10-09 18:37:12 -0300

    teste

--- scripts/index.js
+++ scripts/index.js
@@ -1,4 +1,4 @@
-var formSignin = document.querySelector('#signin')
+/*var formSignin = document.querySelector('#signin')
 var formSignup = document.querySelector('#signup')
 var btnColor = document.querySelector('.btnColor')
 
@@ -14,5 +14,5 @@ document.querySelector('#btnSignup')
     formSignin.style.left = "-450px"
     formSignup.style.left = "25px"
     btnColor.style.left = "110px"
-})
+})*/
 
